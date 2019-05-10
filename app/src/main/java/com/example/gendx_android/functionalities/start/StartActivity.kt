dbdac0c227c75cd5faad83ecf522752f22f1255c
HolkerDev <nick.penkovskiy@gmail.com>
2019-05-10 20:42:23 +0200

    Basic architecture added

---
+++ app/src/main/java/com/example/gendx_android/functionalities/start/StartActivity.kt
@@ -0,0 +1,13 @@
+package com.example.gendx_android.functionalities.start
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gendx_android.R
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
