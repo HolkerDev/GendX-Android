faf74f4650a35bfc68d5eac935d5d66a9a7d4d1a
HolkerDev <nick.penkovskiy@gmail.com>
2019-05-11 10:27:58 +0200

    Checking permission added

--- app/src/main/java/com/example/gendx_android/functionalities/start/StartActivity.kt
+++ app/src/main/java/com/example/gendx_android/functionalities/start/StartActivity.kt
@@ -1,14 +1,21 @@
 package com.example.gendx_android.functionalities.start
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
-import androidx.lifecycle.get
 import com.example.gendx_android.R
 import com.example.gendx_android.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+
+    private val REQUEST_CAMERA = 3;
+
     private lateinit var binding: ActivityStartBinding
     private lateinit var viewModel: StartViewModel
 
@@ -22,9 +29,33 @@ class StartActivity : AppCompatActivity() {
         binding.startViewModel = viewModel
         initObservables()
         //endregion
+
+        checkPermissions()
+
     }
 
     private fun initObservables() {
         //TODO: Init observable for viewModel event
+    }
+
+    private fun checkPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CAMERA) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(applicationContext, "Persmission granted", Toast.LENGTH_LONG).show()
+            } else {
+                finish()
+            }
+        }
     }
 }
